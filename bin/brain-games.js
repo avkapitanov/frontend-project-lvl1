ddca0b080d5e726f413f3171492a4e9a9ad2be22
Artem Kapitanov <avkapitanov@yandex.ru>
2021-01-06 21:30:52 +0300

    Add CodeClimate badge Add linter config Add github actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node node
-import app from "../src/cli.js";
+import app from '../src/cli.js';
 
 app();
--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,17 @@
 import readlineSync from 'readline-sync';
 
 const renderWelcome = () => {
-    console.log(`Welcome to the Brain Games!`);
+  console.log('Welcome to the Brain Games!');
 };
 
 const renderGreeting = (name) => {
-    console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!`);
 };
 
 const askName = () => readlineSync.question('May I have your name? ');
 
 export default () => {
-    renderWelcome();
-    const name = askName();
-    renderGreeting(name);
-}
+  renderWelcome();
+  const name = askName();
+  renderGreeting(name);
+};
